1fc8fa7cd6963b6bc9792efb4c1770543e8fa0f3
Thomas BAGREL <tomsb07@gmail.com>
2022-01-07 08:35:42 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "word-game flake";
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    let
+      overlays = [ haskellNix.overlay
+        (final: prev: {
+          # This overlay adds our project to pkgs
+          wordGameProject =
+            final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc8107";
+              # This is used by `nix develop .` to open a shell for use with
+              # `cabal`, `hlint` and `haskell-language-server`
+              shell.tools = {
+                cabal = {};
+                hlint = {};
+                haskell-language-server = {};
+              };
+              # Non-Haskell shell tools go here
+              shell.buildInputs = with pkgs; [
+                nixpkgs-fmt
+              ];
+            };
+        })
+      ];
+      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+      flake = pkgs.wordGameProject.flake {
+      };
+    in flake // {
+      # Built by `nix build .`
+      defaultPackage = flake.packages."word-game:exe:word-game";
+    });
+}
